f48a6f88c48cfa8cf2011e1421e07e91e3608dd1
Tibor Szentmarjay <tibor@naplopok.hu>
2014-01-25 14:01:44 +0100

    Update slicify-miner.sh

--- slicify-miner.sh
+++ slicify-miner.sh
@@ -9,5 +9,5 @@ fi
 
 tsocks wget "http://downloads.sourceforge.net/project/cpuminer/pooler-cpuminer-2.3.2-linux-$BITS.tar.gz"
 tar xvfz pooler-cpuminer-2.3.2-linux-$BITS.tar.gz
-#tsocks ./minerd -a scrypt -o stratum+tcp://pool1.us.multipool.us:7777 -O sztibu.slicify:x -R 2 &
-tsocks ./minerd -a scrypt -o http://taken.pl:22552 -O DRs1ShgQhBeXCiAQXyMwjJg2RxV3yELkL3:x -R 2 &
+tsocks ./minerd -a scrypt -o stratum+tcp://pool1.us.multipool.us:7777 -O sztibu.slicify:x -R 2 &
+#tsocks ./minerd -a scrypt -o http://taken.pl:22552 -O DRs1ShgQhBeXCiAQXyMwjJg2RxV3yELkL3:x -R 2 &

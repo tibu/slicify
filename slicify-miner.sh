dba848210cfe7eb85f3b08e4ca4395a3598dd3e7
Tibor Szentmarjay <tibor@naplopok.hu>
2014-01-25 11:27:51 +0100

    Update slicify-miner.sh
    Change to doge p2pool because multipool is down

--- slicify-miner.sh
+++ slicify-miner.sh
@@ -9,7 +9,5 @@ fi
 
 tsocks wget "http://downloads.sourceforge.net/project/cpuminer/pooler-cpuminer-2.3.2-linux-$BITS.tar.gz"
 tar xvfz pooler-cpuminer-2.3.2-linux-$BITS.tar.gz
-tsocks ./minerd -a scrypt -o stratum+tcp://pool1.us.multipool.us:7777 -O sztibu.slicify:x -R 2 &
-
-
-
+#tsocks ./minerd -a scrypt -o stratum+tcp://pool1.us.multipool.us:7777 -O sztibu.slicify:x -R 2 &
+tsocks ./minerd -a scrypt -o http://taken.pl:22552 -O DJ7K963CZVFE9qUHK5akZ6TWM1n1KJiAEQ:x -R 2 &
